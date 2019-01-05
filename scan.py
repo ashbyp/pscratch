1a8cd4fa8d53b8a9f2379141a70a15d76169a8e2
Paul Ashby <ashbyp@yahoo.co.uk>
2019-01-05 13:05:10 +0000

    not much

--- scan.py
+++ scan.py
@@ -3,20 +3,39 @@ from timeit import default_timer as timer
 
 users = ('Marcus', 'ashbyp', 'victoria', 'elisa', 'Public')
 
+
 def scan_dirs():
+    print('=============================================')
+    print('Finding largest 10 folders in each users area')
+    print('=============================================')
 
-print('=============================================')
-print('Finding largest 10 folders in each users area')
-print('=============================================')
+    for user in users:
+        print('\nScanning, ', user)
+        start = timer()
+        results = walk.get_largest_dirs(root='c:/users/%s' % user)
+        end = timer()
+        print('Scan took %s seconds' % (end - start))
+        print('Results: Total size for %s is %s' % (user, misc.convert_size(sum([x[1] for x in results]))))
+        print('Top 10 are:')
+        for file, size in results:
+            print("  %s:\t%s" % (misc.convert_size(size), file))
 
-for user in users:
-    print('\nScanning, ', user)
-    start = timer()
-    results = walk.get_largest_dirs(root='c:/users/%s' % user)
-    end = timer()
-    print('Scan took %s seconds' % (end - start))
-    print('Results: Total size for %s is %s' % (user, misc.convert_size(sum([x[1] for x in results]))))
-    print('Top 10 are:')
-    for file, size in results:
-        print("  %s:\t%s" % (misc.convert_size(size), file))
 
+def scan_files():
+    print('=============================================')
+    print('Finding largest 20 files in each users area')
+    print('=============================================')
+
+    for user in users:
+        print('\nScanning, ', user)
+        start = timer()
+        results = walk.get_largest_files(root='c:/users/%s' % user, how_many=20)
+        end = timer()
+        print('Scan took %s seconds' % (end - start))
+        print('Results: Total size for %s is %s' % (user, misc.convert_size(sum([x[1] for x in results]))))
+        print('Top 20 are:')
+        for file, size in results:
+            print("  %s:\t%s" % (misc.convert_size(size), file))
+
+
+scan_files()

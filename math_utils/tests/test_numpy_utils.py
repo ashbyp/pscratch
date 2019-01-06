c5d58309c16eeb0193061e8cece5302141960511
ashbyp <r0sebud>
2019-01-06 11:29:57 +0000

    missed quote

---
+++ math_utils/tests/test_numpy_utils.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from math_utils import numpy_utils
+
+
+class TestAllUtils(TestCase):
+
+    def test_get_random_int_array(self):
+        a = numpy_utils.get_random_int_array((3, 4, 5), 0)
+        self.assertEqual(a.ndim, 3)
+        self.assertEqual(a.shape, (3, 4, 5))
+        self.assertEqual(a.size, 60)
+
+
+
+

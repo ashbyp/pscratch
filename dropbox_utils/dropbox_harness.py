77296026c7cb5efacc871aa313d687d5d08243c6
ashbyp <r0sebud>
2019-01-27 12:47:22 +0000

    renaming stuff

--- dropbox_utils/dropbox_harness.py
+++ dropbox_utils/dropbox_harness.py
@@ -3,7 +3,10 @@ import dropbox
 TOK = 'YzhCOnmeCxcAAAAAAAAXJCYc57j6CHEgMH2XZ4_gale3nwuVQ8RsEY12uYbKTJXA'
 
 dbx = dropbox.Dropbox(TOK)
-print(dbx.users_get_current_account())
 
-print(dbx.file_requests_list())
+for r in dbx.files_list_folder('').entries:
+    print(r.path_display)
+
+
+
 

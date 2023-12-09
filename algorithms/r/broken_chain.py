a1bb8e4f3a664549f6fcfbe3c3e1088ea0487035
ashbyp <ashbyp@yahoo.co.uk>
2023-12-09 09:01:50 +0000

    chocolate

--- algorithms/r/broken_chain.py
+++ algorithms/r/broken_chain.py
@@ -1,5 +1,5 @@
 from utils.measure import checker
-
+import math
 
 def solution(A):
     min_cost = float('inf')
@@ -11,10 +11,34 @@ def solution(A):
                 min_cost = min(min_cost, break_cost)
     return min_cost
 
+
+
+def minSum(array):
+    _min = array[1]
+    result = math.inf
+
+    print(array)
+
+    for i in range(3, len(array) - 1):
+        print('--------')
+        print('i=', i)
+        print('array[i-2]', array[i-2])
+        _min = min(_min, array[i-2])
+        print('_min', _min)
+        print('array[i]', array[i])
+        if (_min + array[i]) < result:
+            result = _min + array[i]
+            print('result', result)
+        print()
+    return result
+
 # Example usage:
 A = [5, 2, 4, 6, 3, 7]
 print(solution(A))
 
-with checker(solution, repeat=0, check_success=True) as c:
-    c.check_1([5, 2, 4, 6, 3, 7] + [1000] * 1000, 5)
+# with checker(solution, repeat=1000, check_success=True) as c:
+#     c.check_1([5, 2, 4, 6, 3, 7] + [1000] * 1000, 5)
 
+with checker(minSum, repeat=0, check_success=True) as c:
+    # c.check_1([5, 2, 4, 6, 3, 7] + [1000] * 1000, 5)
+    c.check_1([5, 2, 4, 6, 3, 7], 5)

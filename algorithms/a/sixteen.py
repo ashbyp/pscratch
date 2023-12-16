f5a1ed6ba3229b7d807bbf53b6671970871de56a
ashbyp <ashbyp@yahoo.co.uk>
2023-12-16 13:40:02 +0000

    bread

--- algorithms/a/sixteen.py
+++ algorithms/a/sixteen.py
@@ -1,6 +1,3 @@
-from dataclasses import dataclass
-
-
 def read_data(filename) -> str:
     with open(filename) as f:
         return f.read().strip()
@@ -8,66 +5,52 @@ def read_data(filename) -> str:
 
 # data=read_data('16_test_data.txt')
 # data=read_data('16_my_test_data.txt')
-data=read_data('16_puzzle_data.txt')
-
+data = read_data('16_puzzle_data.txt')
 
 UP = 1
 DOWN = 2
 LEFT = 3
 RIGHT = 4
 
-
-@dataclass(frozen=True)
-class Sprite:
-    row: int
-    col: int
-    direction: int
-
-    move_map = {
-        UP: (-1, 0),
-        DOWN: (1, 0),
-        LEFT: (0, -1),
-        RIGHT: (0, 1),
-    }
-
-    @staticmethod
-    def move(sprite: 'Sprite', direction: int):
-        move = Sprite.move_map[direction]
-        return Sprite(sprite.row + move[0], sprite.col + move[1], direction)
-
+MOVE_MAP = {
+    UP: (-1, 0),
+    DOWN: (1, 0),
+    LEFT: (0, -1),
+    RIGHT: (0, 1),
+}
 
 OBSTACLE_MAP = {
-        '.': {
-                UP:    [UP],
-                DOWN:  [DOWN],
-                LEFT:  [LEFT],
-                RIGHT: [RIGHT],
-        },
-        '/': {
-                UP:    [RIGHT],
-                DOWN:  [LEFT],
-                LEFT:  [DOWN],
-                RIGHT: [UP],
-        },
-        '\\': {
-                UP:    [LEFT],
-                DOWN:  [RIGHT],
-                LEFT:  [UP],
-                RIGHT: [DOWN],
-        },
-        '|': {
-            UP: [UP],
-            DOWN: [DOWN],
-            LEFT: [UP, DOWN],
-            RIGHT: [UP, DOWN],
-        },
-        '-': {
-            UP: [LEFT, RIGHT],
-            DOWN: [LEFT, RIGHT],
-            LEFT: [LEFT],
-            RIGHT: [RIGHT],
-        },
-    }
+    '.': {
+        UP: [UP],
+        DOWN: [DOWN],
+        LEFT: [LEFT],
+        RIGHT: [RIGHT],
+    },
+    '/': {
+        UP: [RIGHT],
+        DOWN: [LEFT],
+        LEFT: [DOWN],
+        RIGHT: [UP],
+    },
+    '\\': {
+        UP: [LEFT],
+        DOWN: [RIGHT],
+        LEFT: [UP],
+        RIGHT: [DOWN],
+    },
+    '|': {
+        UP: [UP],
+        DOWN: [DOWN],
+        LEFT: [UP, DOWN],
+        RIGHT: [UP, DOWN],
+    },
+    '-': {
+        UP: [LEFT, RIGHT],
+        DOWN: [LEFT, RIGHT],
+        LEFT: [LEFT],
+        RIGHT: [RIGHT],
+    },
+}
 
 
 def shine_beam(grid, start_pos):
@@ -76,25 +59,26 @@ def shine_beam(grid, start_pos):
     queue = [start_pos]
 
     while len(queue):
-        current_pos = queue.pop()
-        if not (0 <= current_pos.row < n_rows and 0 <= current_pos.col < n_cols) or current_pos in illuminated:
+        pos = queue.pop()
+        if not (0 <= pos[0] < n_rows and 0 <= pos[1] < n_cols) or pos in illuminated:
             continue
 
-        illuminated.add(current_pos)
-        obstacle = grid[current_pos.row][current_pos.col]
+        illuminated.add(pos)
+        obstacle = grid[pos[0]][pos[1]]
 
         directions = OBSTACLE_MAP[obstacle]
-        for direction in directions[current_pos.direction]:
-            queue.append(Sprite.move(current_pos, direction))
+        for direction in directions[pos[2]]:
+            move = MOVE_MAP[direction]
+            queue.append((pos[0] + move[0], pos[1] + move[1], direction))
 
-    return len(set((s.row, s.col) for s in illuminated))
+    return len(set((i[0], i[1]) for i in illuminated))
 
 
 def part1():
     grid = list(map(list, data.splitlines()))
     n_rows, n_cols = len(grid), len(grid[0])
     print(f'Rows {n_rows}, Cols {n_cols}')
-    print(shine_beam(grid, (Sprite(0, 0, RIGHT))))
+    print(shine_beam(grid, (0, 0, RIGHT)))
 
 
 def part2():
@@ -105,19 +89,20 @@ def part2():
     max_illuminated = 0
 
     for r in range(n_rows):
-        max_illuminated = max(max_illuminated, shine_beam(grid, (Sprite(r, 0, RIGHT))))
-        max_illuminated = max(max_illuminated, shine_beam(grid, (Sprite(r, n_cols - 1, LEFT))))
+        max_illuminated = max(max_illuminated, shine_beam(grid, (r, 0, RIGHT)))
+        max_illuminated = max(max_illuminated, shine_beam(grid, (r, n_cols - 1, LEFT)))
 
     for c in range(n_cols):
-        max_illuminated = max(max_illuminated, shine_beam(grid, (Sprite(0, c, DOWN))))
-        max_illuminated = max(max_illuminated, shine_beam(grid, (Sprite(n_rows - 1, c, UP))))
+        max_illuminated = max(max_illuminated, shine_beam(grid, (0, c, DOWN)))
+        max_illuminated = max(max_illuminated, shine_beam(grid, (n_rows - 1, c, UP)))
 
     print(max_illuminated)
 
 
 if __name__ == '__main__':
     from utils.measure import timeblock
+
     with timeblock():
         part1()
-    with timeblock():
+    with timeblock():  # 7 secs
         part2()

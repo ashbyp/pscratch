7a768c47cd6a2b14496bd11beb9b5b85915ca4c1
ashbyp <ashbyp@yahoo.co.uk>
2024-04-07 14:08:02 +0100

    bad bad back

--- algorithms/aoc/2016/14.py
+++ algorithms/aoc/2016/14.py
@@ -1,3 +1,5 @@
+import functools
+
 question = "https://adventofcode.com/2016/day/14"
 input_data = "https://adventofcode.com/2016/day/14/input"
 
@@ -7,7 +9,19 @@ tdata = """abc"""
 
 
 def part1(data: str, debug: bool = False):
-    pass
+    CACHE.clear()
+    found = 0
+    cache = {}
+    i = 0
+    while found < 64:
+        s = data + str(i)
+        v = hhhh(s, 0)
+        t = has_triple(v)
+        if t:
+            if check(t, data, cache, i + 1, 0):
+                found += 1
+        i += 1
+    print(i - 1)
 
 
 def has_triple(h: str):
@@ -24,54 +38,50 @@ def all_fives(h: str):
             s.add(h[i])
     return s
 
+
 CACHE = {}
 
-def hhhh(s):
-    if s in CACHE:
-        return CACHE[s]
+def hhhh(s, repeat):
     import hashlib
+    if s in CACHE: return CACHE[s]
     h = s.encode("utf-8")
     h = hashlib.md5(h).hexdigest()
-    # print(h)
-    for _ in range(2016):
+    for _ in range(repeat):
+        # print('x')
         h = h.encode("utf-8")
         h = hashlib.md5(h).hexdigest()
         # print(h)
     CACHE[s] = h
     return h
 
-def check(triple, salt, cache, idx):
-    # print('check', triple, 'at', idx-1)
-    import hashlib
-    # print('check', triple, 'at', idx)
+
+def check(triple, salt, cache, idx, repeat):
     for tries in range(1000):
         fives = cache.get(idx + tries)
         if fives is None:
             s = salt + str(idx + tries)
-            v = hhhh(s)
+            v = hhhh(s, repeat)
             fives = all_fives(v)
             cache[idx] = fives
         if triple in fives:
-            print('  found at ', idx+tries)
             return True
     return False
 
 
 def part2(data: str, debug: bool = False):
-    import hashlib
+    CACHE.clear()
     found = 0
     cache = {}
     i = 0
     while found < 64:
         s = data + str(i)
-        v = hhhh(s)
+        v = hhhh(s, 2016)
         t = has_triple(v)
         if t:
-            if check(t, data, cache, i + 1):
+            if check(t, data, cache, i + 1, 2016):
                 found += 1
-                print('found', found)
         i += 1
-    print(i-1)
+    print(i - 1)
 
 
 def main():
@@ -85,11 +95,11 @@ def main():
             elapsed_ms = (timeit.default_timer() - start) * 1000
             print(f'Time {elapsed_ms:.10f} ms ------------')
 
-    # run("Part 1 test", tdata, part1, True)
-    # run("Part 1 puzzle", pdata, part1, False)
-    # run("Part 2 test", tdata, part2, True)
+    run("Part 1 test", tdata, part1, True)
+    run("Part 1 puzzle", pdata, part1, False)
+    run("Part 2 test", tdata, part2, True)
     run("Part 2 puzzle", pdata, part2, False)
+
 
 if __name__ == '__main__':
     main()
-    # print(hhhh('abc0'))
